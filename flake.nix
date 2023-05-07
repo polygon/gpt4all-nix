b8347857cb24658f08a6b287b3c6531199e4fbb9
Ilan Joselevich <personal@ilanjoselevich.com>
2023-05-07 22:59:38 +0300

    refactor

--- flake.nix
+++ flake.nix
@@ -1,28 +1,22 @@
 {
-  description = "A very basic flake";
-
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.gpt4all-chat = {
-    url = "https://github.com/nomic-ai/gpt4all-chat";
-    flake = false;
-    submodules = true;
-    type = "git";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts = { url = "github:hercules-ci/flake-parts"; inputs.nixpkgs-lib.follows = "nixpkgs"; };
+    src = { url = "https://github.com/nomic-ai/gpt4all-chat"; flake = false; submodules = true; type = "git"; };
   };
 
-  outputs = { self, nixpkgs, gpt4all-chat }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    packages.${system} = {
-      gpt4all-chat = pkgs.qt6Packages.callPackage ./gpt4all-chat.nix { src=gpt4all-chat; };
-      default = self.packages.${system}.gpt4all-chat;
-    };
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
 
-    apps.${system}.default = {
-      type = "app";
-      program = "${self.packages.${system}.gpt4all-chat}/bin/chat";
+      perSystem = { pkgs, config, ... }: {
+        packages = {
+          gpt4all-chat = pkgs.qt6Packages.callPackage ./. {
+            inherit (inputs) src;
+            version = builtins.concatStringsSep "-" (builtins.match "([0-9]{4})([0-9]{2})([0-9]{2})[0-9]*" inputs.src.lastModifiedDate);
+          };
+          default = config.packages.gpt4all-chat;
+        };
+      };
     };
-  };
 }
